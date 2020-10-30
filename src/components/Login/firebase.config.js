e1ed2424181e3cece59f1f3fce919f9427adacf9
Jobayed-Shakib <71041987+Jobayed-Shakib@users.noreply.github.com>
2020-10-30 14:40:22 +0600

    Initial commit

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDl3lif58IJ81ilCIKVOZKA9i96fjLceGw",
+  authDomain: "burj-al-arab-54635.firebaseapp.com",
+  databaseURL: "https://burj-al-arab-54635.firebaseio.com",
+  projectId: "burj-al-arab-54635",
+  storageBucket: "burj-al-arab-54635.appspot.com",
+  messagingSenderId: "731346003368",
+  appId: "1:731346003368:web:7ad3b3b18bb9432b88a99d"
+};
+
+  export default firebaseConfig;
